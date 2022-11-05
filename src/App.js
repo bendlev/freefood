fbc9e386434ef503394b38e40f4fa50c92d715d6
ashleyliangg <102703391+ashleyliangg@users.noreply.github.com>
2022-11-05 16:45:15 -0400

    added test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
@@ -12,20 +13,7 @@ function App() {
   return (
     //react boilerplate
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>this is a test</p>
     </div>
 
   );
