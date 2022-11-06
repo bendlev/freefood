74f15224b7d12ce1ca2188655bdc27c2f558ec1f
Ben Levesque <104855043+bendlev@users.noreply.github.com>
2022-11-06 10:03:59 -0500

    Final Push!

--- src/App.js
+++ src/App.js
@@ -13,9 +13,55 @@ function App() {
   return (
 
     <div className="App">
-      <p>this is a test</p>
+
+      <body>
+        <div id="banner">
+          <div id="banner-content">
+          FREEFOOD@NOW
+          </div>
+        </div>
+        
+        <div id="main-content">
+          <div class="block1">
+            <div id="header">
+              Have Free Food Around You?
+            </div>
+            <div id="main">
+              Maybe you do, maybe you don't.
+            </div>
+            <div id="third">
+              explanations explanations explanations explanations explanations explanations explanations explanations explanations explanations explanations explanations. 
+            </div>
+            <div id="fourth" class="myButton">
+              Share Free Food Info Here
+            </div>
+          </div>
+        <div class="tiles">
+          <Card name="TukTuk" location="ECSC 116" servingsize="20" tags="#vegetarian"/>
+          <Card name="TukTuk" location="ECSC 116" servingsize="20" tags="#vegetarian"/>
+          <Card name="TukTuk" location="ECSC 116" servingsize="20" tags="#vegetarian"/>
+          {/* <div class="tile">Tile One</div>
+          <div class="tile">Tile Two</div>
+          <div class="tile">Tile Three</div> */}
+        </div>
+        <div class="click-container">
+          <div class="click-div">
+            1
+          </div>
+          <div class="click-div">
+            2
+          </div>
+          <div class="click-div">
+            3
+          </div>
+        </div>
+      </div>
+      </body>
+
       {/* each card must include name, location, servings, and tags */}
-      <Card name="TukTuk" location="ECSC 116" servingsize="20" tags="#vegetarian"/>
+
+
+      
     </div>
 
   );
